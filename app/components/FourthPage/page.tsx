3136cf4f0541615c0751f14ec2274ea7a48994d6
Himanshu4252 <himanshuhddudhpuri4252@gmail.com>
2024-09-04 16:49:08 +0530

    updated skill section

--- app/components/FourthPage/page.tsx
+++ app/components/FourthPage/page.tsx
@@ -1,21 +1,72 @@
 import React from 'react'
 import style from './style.module.css'
+import {reactLogo, nextLogo, javaScriptLogo, htmlLogo, cssLogo, cLogo, cppLogo, cSharpLogo, bootstrapLogo, dsaLogo, tsLogo, githubLogo} from './photo'
+import Image from 'next/image'
+
 const Fourthpage = () => {
   return (<div className={style.forthWrapper}>
     <div className={style.topText}>My Skills</div>
     <div className={style.skillSection}>
-        <div className={style.skill}>NextJs</div>
-        <div className={style.skill}>ReactJs</div>
-        <div className={style.skill}>JavaScript</div>
-        <div className={style.skill}>TypeScript</div>
-        <div className={style.skill}>C</div>
-        <div className={style.skill}>C++</div>
-        <div className={style.skill}>Git/GitHUB</div>
-        <div className={style.skill}>HTML</div>
-        <div className={style.skill}>CSS</div>
-        <div className={style.skill}>Data structures and Algorithms</div>
-        <div className={style.skill}>BootStrap</div>
-        <div className={style.skill}>C#</div>
+        <div className={style.skill}>
+          <Image src={nextLogo} alt='nextjs LOGO' className={style.skillLogos} />
+          NextJs
+        </div>
+
+        <div className={style.skill}>
+        <Image src={reactLogo} alt='reactjs logo' className={style.skillLogos} />
+        ReactJs
+        </div>
+
+        <div className={style.skill}>
+        <Image src={javaScriptLogo} alt='java script logo' className={style.skillLogos} />
+        JavaScript
+        </div>
+
+        <div className={style.skill}>
+        <Image src={tsLogo} alt='typescript logo' className={style.skillLogos} />
+        TypeScript
+        </div>
+
+        <div className={style.skill}>
+        <Image src={cLogo} alt='c Logo' className={style.skillLogos} />
+        C
+        </div>
+
+        <div className={style.skill}>
+        <Image src={cppLogo} alt='C++ logo' className={style.skillLogos} />
+        C++
+        </div>
+
+        <div className={style.skill}>
+        <Image src={githubLogo} alt='git hub Logo' className={style.skillLogos} />
+        Git/GitHUB
+        </div>
+
+        <div className={style.skill}>
+        <Image src={htmlLogo} alt='html logo' className={style.skillLogos} />
+        HTML
+        </div>
+
+        <div className={style.skill}>
+        <Image src={cssLogo} alt=' css logo ' className={style.skillLogos} />
+        CSS
+        </div>
+
+        <div className={style.skill}>
+        <Image src={dsaLogo} alt='data structures and algorithms' className={style.skillLogos} />
+        Data structures and Algorithms
+        </div>
+
+        <div className={style.skill}>
+        <Image src={bootstrapLogo} alt='Bootstrap Logo' className={style.skillLogos} />
+        BootStrap
+        </div>
+
+        <div className={style.skill}>
+        <Image src={cSharpLogo} alt='C sharp Logo' className={style.skillLogos} />
+        C#
+        </div>
+
     </div>
   </div>
   )
---
+++ app/components/FourthPage/photo.ts
@@ -0,0 +1,14 @@
+import reactLogo from './photos/react.png'
+import nextLogo from './photos/nextjs.png'
+import javaScriptLogo from './photos/js.png'
+import htmlLogo from './photos/html.png'
+import cssLogo from './photos/css.png'
+import cLogo from './photos/c.png'
+import cppLogo from './photos/cpp.png'
+import cSharpLogo from './photos/csharp.png'
+import bootstrapLogo from './photos/bootstrap.png'
+import dsaLogo from './photos/structures.png'
+import tsLogo from './photos/ts.png'
+import githubLogo from './photos/github.png'
+ 
+export {reactLogo, nextLogo, javaScriptLogo, htmlLogo, cssLogo, cLogo, cppLogo, cSharpLogo, bootstrapLogo, dsaLogo, tsLogo, githubLogo}
